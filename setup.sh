304c527fac32392df4d2bf023314514fedf4c409
Rockz1152 <jwheeler1152@gmail.com>
2021-09-11 19:02:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,69 +1,87 @@
 #!/bin/bash
 
-# OS Check
-if [[ `cat /etc/debian_version 2>/dev/null | grep -E '11.|10.|9.' | wc -l` == 1 ]]; then
-    echo ''
-    echo 'Provisioning Debian'
-    echo ''
-else
-    echo 'This script is designed to only run on Debian 9, 10, or 11'
-    exit 1
-fi
+# Setup Script for Debian 9, 10, and 11
+# https://github.com/Rockz1152/Debian
+
+function checkOS() {
+    # Check OS version
+    if [[ -e /etc/debian_version ]]; then
+        source /etc/os-release
+        if [[ ${ID} == "debian" || ${ID} == "raspbian" ]]; then
+            if [[ ${VERSION_ID} -lt 9 ]]; then
+                echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 8 Jessie or later"
+                exit 1
+            fi
+        fi
+    else
+        echo "Looks like you aren't running this installer on a supported Debian install"
+        exit 1
+    fi
+}
 
 # Check for root, otherwise re-run
-[ `whoami` = root ] || { sudo "$0" "$@"; exit $?; }
+#[ `whoami` = root ] || { sudo "$0" "$@"; exit $?; }
 
-# Apt update and upgrade
-echo 'Running apt-update'
-apt-get -q update > /dev/null
-echo 'Running apt-upgrade'
-apt-get -q -y dist-upgrade > /dev/null
-
-# Software to install
-echo 'Installing software'
-declare -a packages=('curl' 'nano' 'vim' 'htop' 'ncdu' 'wget' 'zip' 'unzip' 'p7zip-full' 'screen' 'less' 'man-db' 'neofetch')
-
-# Check for VMware
-if [[ `dmidecode -t system | grep Manufacturer | awk '{sub(/Manufacturer:/,"");print}' | xargs` == "VMware, Inc." ]]; then
-    packages=(${packages[@]} "open-vm-tools")
-fi
-# Check for QEMU
-if [[ `dmidecode -t system | grep Manufacturer | awk '{sub(/Manufacturer:/,"");print}' | xargs` == "QEMU" ]]; then
-    packages=(${packages[@]} "qemu-guest-agent")
-fi
-
-# Install software
-for i in "${packages[@]}"
-do
-    echo '-'$i
-    if [[ `dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed"` == 0 ]]; then
-        apt-get -q -y install $i > /dev/null 2>/dev/null
-        # Verify install
-        if [[ `dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed"` == 0 ]]; then
-            echo '!!!' $i 'failed to install'
-        fi
+function isRoot() {
+    if [ "${EUID}" -ne 0 ]; then
+        echo "You need to run this script as root"
+        exit 1
     fi
-done
+}
 
-# Disable motd on ssh login
-if [ -f /etc/pam.d/sshd ]; then
-echo 'Turning off SSH motd'
-sudo sed -i 's@.*session    optional     pam_motd.so  motd=/run/motd.dynamic@#session    optional     pam_motd.so  motd=/run/motd.dynamic@' /etc/pam.d/sshd
-sudo sed -i 's@.*session    optional     pam_motd.so noupdate@#session    optional     pam_motd.so noupdate@' /etc/pam.d/sshd
-fi
+function InstallSoftware() {
+    # Apt update and upgrade
+    echo 'Running apt-update'
+    apt-get -q update > /dev/null
+    echo 'Running apt-upgrade'
+    apt-get -q -y dist-upgrade > /dev/null
 
-# Configure vim
-echo 'Configuring Vim'
+    # Software to install
+    echo 'Installing software'
+    declare -a packages=('curl' 'nano' 'vim' 'htop' 'ncdu' 'wget' 'zip' 'unzip' 'p7zip-full' 'screen' 'less' 'man-db' 'neofetch')
 
-if [[ `cat /etc/debian_version 2>/dev/null | grep 11.` ]]; then
-    echo "source /usr/share/vim/vim82/defaults.vim" > /root/.vimrc
-elif [[ `cat /etc/debian_version 2>/dev/null | grep 10.` ]]; then
-    echo "source /usr/share/vim/vim81/defaults.vim" > /root/.vimrc
-elif [[ `cat /etc/debian_version 2>/dev/null | grep 9.` ]]; then
-    echo "source /usr/share/vim/vim80/defaults.vim" > /root/.vimrc
-fi
+    # Check for VMware
+    if [ "$(systemd-detect-virt)" == "vmware" ]; then
+      packages=(${packages[@]} "open-vm-tools")
+    fi
 
-cat >> /root/.vimrc << EOF
+    # Check for QEMU
+    if [ "$(systemd-detect-virt)" == "qemu" ]; then
+      packages=(${packages[@]} "qemu-guest-agent")
+    fi
+
+    # Install software
+    for i in "${packages[@]}"
+    do
+        echo '-'$i
+        if [[ `dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed"` == 0 ]]; then
+            apt-get -q -y install $i > /dev/null 2>/dev/null
+            # Verify install
+            if [[ `dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed"` == 0 ]]; then
+                echo '!!!' $i 'failed to install'
+            fi
+        fi
+    done
+}
+
+function SSHmotd() {
+    if [ -f /etc/pam.d/sshd ]; then
+        echo 'Turning off SSH motd'
+        sudo sed -i 's@.*session    optional     pam_motd.so  motd=/run/motd.dynamic@#session    optional     pam_motd.so  motd=/run/motd.dynamic@' /etc/pam.d/sshd
+        sudo sed -i 's@.*session    optional     pam_motd.so noupdate@#session    optional     pam_motd.so noupdate@' /etc/pam.d/sshd
+    fi
+}
+
+function ConfigVIM() {
+    echo 'Configuring Vim'
+    # if [[ `cat /etc/debian_version 2>/dev/null | grep 11.` ]]; then
+        # echo "source /usr/share/vim/vim82/defaults.vim" > /root/.vimrc
+    # elif [[ `cat /etc/debian_version 2>/dev/null | grep 10.` ]]; then
+        # echo "source /usr/share/vim/vim81/defaults.vim" > /root/.vimrc
+    # elif [[ `cat /etc/debian_version 2>/dev/null | grep 9.` ]]; then
+        # echo "source /usr/share/vim/vim80/defaults.vim" > /root/.vimrc
+    # fi
+cat > /etc/vim/vimrc.local << EOF
 colorscheme desert
 set nowrap
 
@@ -73,10 +91,11 @@ if has('mouse')
  set mouse=r
 endif
 EOF
+}
 
-# Configure bash
-echo 'Configuring Bash'
-\cp /etc/skel/.bashrc /root/.bashrc
+function ConfigBash() {
+    echo 'Configuring Bash'
+    \cp /etc/skel/.bashrc /root/.bashrc
 cat >> /root/.bashrc << EOF
 
 alias reboot='/sbin/reboot'
@@ -88,22 +107,53 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias grep='grep --color=auto'
 EOF
-
-# Configure bash input
+# Configure bash input, Debian 11+ only
+if [[ ${VERSION_ID} -ge 11 ]]; then
 cat > /root/.inputrc << EOF
 \$include /etc/inputrc
 set enable-bracketed-paste Off
 EOF
+fi
+}
 
-# Copy dotfiles into home directories
-for d in `find /home/* -maxdepth 0 -type d`
-do
-    \cp /root/.bashrc $d
-    \cp /root/.vimrc $d
-    \cp /root/.inputrc $d
-done
+function Movedotfiles() {
+    for d in /home/* ;
+    do
+        \cp /root/.bashrc $d
+        # \cp /root/.vimrc $d
+        if [ -f /root/.inputrc ]; then
+            \cp /root/.inputrc $d
+        fi
+    done
+}
 
-# Reload bash for sudo user
-echo 'Reloading Bash'
-echo ''
-# sudo -u $SUDO_USER sh -c "exec bash"
+function ReloadBash() {
+    echo 'Reloading Bash'
+    echo ''
+    source ~/.bashrc
+    # sudo -u $SUDO_USER sh -c "exec bash"
+}
+
+CheckOS
+CheckRoot
+
+function Main() {
+    InstallSoftware
+    SSHmotd
+    ConfigVIM
+    ConfigBash
+    Movedotfiles
+    ReloadBash
+}
+
+# Check if system was already provisioned
+if [[ -e /root/.bashrc ]]; then
+    read -p "Run setup again?" yn
+    case $yn in
+        [Yy]* ) Main; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+else
+    Main
+fi
