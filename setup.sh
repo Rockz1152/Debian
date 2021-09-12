0bc62efe7545a93041ab68ad7411cc49de87c63a
Rockz1152 <jwheeler1152@gmail.com>
2021-09-11 20:57:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -114,7 +114,7 @@ EOF
 function reloadBash() {
     echo 'Reloading Bash'
     echo ''
-    sudo -u $(whoami) sh -c "source /etc/profile"
+    sudo -u $(whoami) bash -c "source /etc/profile"
 }
 
 checkOS
